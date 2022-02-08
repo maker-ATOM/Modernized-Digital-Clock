97b4d5a65495e6a65f6f6a1dd979d529db9dba63
ATOM <87944335+maker-ATOM@users.noreply.github.com>
2022-02-08 09:25:20 +0530

    Add files via upload

---
+++ Sketch/Test files/1_tm1637_test.ino
@@ -0,0 +1,33 @@
+#include <TM1637Display.h>
+
+#define CLK 5
+#define DIO 4
+
+TM1637Display display = TM1637Display(CLK, DIO);
+
+int i = 0, j = 0, k = 0, l = 0;
+
+void setup()
+{
+
+}
+
+void loop()
+{
+  display.setBrightness(0x0a);
+
+  for (l = 0; l < 9; l++)
+  {
+    for (k = 0; k <  9; k++ )
+    {
+      for (j = 0; j < 9; j++)
+      {
+        for (i = 0; i <= 9; i++)
+        {
+          display.showNumberDecEx(l * 1000 + k * 100 + j * 10 + i, B01000000, true, 4, 0);
+          delay(500);
+        }
+      }
+    }
+  }
+}
