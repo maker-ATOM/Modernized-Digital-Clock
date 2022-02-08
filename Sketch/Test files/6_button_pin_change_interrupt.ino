a7cf278105e893ba763af8560b75349fe5da8bf2
ATOM <87944335+maker-ATOM@users.noreply.github.com>
2022-02-08 09:43:29 +0530

    Update 6_button_pin_change_interrupt.ino

--- Sketch/Test files/6_button_pin_change_interrupt.ino
+++ Sketch/Test files/6_button_pin_change_interrupt.ino
@@ -1,35 +1,25 @@
+//pin change interrupt attached to pin A0 and driven by button of encoder
+
 volatile byte button = 0;
-byte a = 0;
 
 void setup()
 {
   PCICR |= B00000010;
   PCMSK1 |= B00000001;
   pinMode(13, OUTPUT);
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
-  Serial.begin(9600);
 }
 
 void loop()
 {
-  if (button == 2)
-  {
-    a = !a;
-    digitalWrite(13, a);
-    button = 3;
-  }
-  Serial.print(digitalRead(2));
-  Serial.print(" ");
-  Serial.print(digitalRead(3));
-  Serial.print(" ");
-  Serial.println(button);
+
+
+    digitalWrite(13, button);
 }
 
 ISR (PCINT1_vect)
 {
   if (!digitalRead(A0))
   {
-    button = 2;
+    button = !button;
   }
 }
